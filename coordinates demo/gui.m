79c6a2f900d99e8fb0c621a95c1069b76ee81994
Ralph Nothdurft <150295967+NothdurftNerdworks@users.noreply.github.com>
2025-01-17 13:21:23 -0600

    Create gui.m

---
+++ coordinates demo/gui.m
@@ -0,0 +1,155 @@
+classdef gui < matlab.mixin.SetGet
+    %GUI Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    %% --- PROPERTIES -----------------------------------------------------
+    properties %(SetAccess = immutable)
+        appName     string  = "My App"
+        appVersion  string  = "0.0"
+        verbose     logical = true  % if TRUE then interactions are announced to stdout     
+
+    end
+
+    properties (Dependent, AbortSet)
+        mouseCoordsVisible logical % toggle display of mouse coordinates (primarily for dev/debug)
+
+    end
+
+    properties %(SetAccess = private)
+        uifig matlab.ui.Figure = matlab.ui.Figure.empty
+        components struct = struct() % struct to hold the various gui components
+
+    end
+
+    %% --- EVENTS ---------------------------------------------------------
+    events
+        UserInteraction
+
+    end
+    
+    %% --- METHODS --------------------------------------------------------
+    methods % constructor/destructor
+        function obj = gui
+            % build the gui
+            obj.makefigure
+            obj.addcomponents
+            obj.layout
+
+            % make visible
+            obj.uifig.Visible = "on";
+
+            % turn on mouse coords
+            obj.mouseCoordsVisible = true;
+
+        end
+
+        function delete(obj)
+
+
+        end
+
+        function closegui(obj, varargin)
+            % close uifigure if it exists
+            if ishandle(obj.uifig) && strcmp(get(obj.uifig, 'type'), 'figure')
+                delete(obj.uifig)
+
+            end
+
+            % now delete the object
+            obj.delete
+
+        end
+
+    end % constructor/destructor
+
+    methods % get/set
+        function value = get.mouseCoordsVisible(obj)
+                value = isfield(obj.components, 'mousecoords');
+
+        end % get.mouseCoordsVisible
+
+        function set.mouseCoordsVisible(obj, value)
+            if value == true
+                disp('turn on mouse coords')
+                % make coords location visible
+                obj.components.mousecoords = uitextarea( ...
+                    parent          = obj.uifig, ...
+                    Position        = [5 5 110 20], ...
+                    BackgroundColor = [0.81 1.00 0.02], ...
+                    FontName        = 'Courier', ...
+                    FontSize        = 12, ...
+                    WordWrap        = 'off', ...
+                    Editable        = 'off', ...
+                    BusyAction      = 'cancel');
+
+                % turn on callback
+                obj.uifig.WindowButtonMotionFcn = {@dispmousecoords, obj};               
+
+            else
+                disp('turn off mouse coords')
+                % turn off callback
+                obj.uifig.WindowButtonMotionFcn = '';
+
+                % make coords location hidden
+                delete(obj.components.mousecoords);
+
+            end
+
+            function dispmousecoords(~, ~, obj)
+                cursorPos = obj.uifig.CurrentPoint;
+                locString = sprintf('X: %3d Y: %3d', cursorPos(1), cursorPos(2));
+                obj.components.mousecoords.Value = locString;
+
+            end % dispmousecoords
+
+        end % set.mouseCoordsVisible
+
+    end % get/set
+
+    methods (Access = private) % private
+        function makefigure(obj)
+            % figure particulars
+            figWidth = 400;
+            figHeight = 400;
+            resizeStatus = "on";
+
+            % coordinates to center the figure
+            screenPos = get(0,'ScreenSize');
+            wScreen = screenPos(3);
+            hScreen = screenPos(4);
+            left = wScreen/2 - figWidth/2;
+            bottom = hScreen/2 - figHeight/2;
+
+            % create main figure
+            obj.uifig = uifigure( ...
+                Visible         = "off", ...
+                CloseRequestFcn = @obj.closegui, ...                % closing figure also deletes the object
+                DeleteFcn       = @obj.closegui, ...                % if figure is destroyed by other means, delete the object
+                Name            = strcat(obj.appName, " ", obj.appVersion), ...
+                Position        = [left bottom figWidth, figHeight], ...
+                Resize          = resizeStatus, ...
+                AutoResizeChildren = 'off', ...
+                SizeChangedFcn  = @obj.layout);
+
+        end % makefigure
+
+        function addcomponents(obj)
+
+        end % addcomponents
+
+        function layout(obj, ~, ~)
+
+        end % layout
+
+    end % private
+
+    methods (Static)
+        function killfigures
+            close all force
+
+        end % killfigures
+
+    end % static
+
+end
+
